9e52c38fd9b57a1bfa3ee86929b2062d63117a83
williamyang-zzu <1654263161@qq.com>
2023-06-02 23:50:18 +0800

    init monet

---
+++ src/main/java/com/fleeting/monet/MonetApplication.java
@@ -0,0 +1,13 @@
+package com.fleeting.monet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fleeting/monet/MonetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleeting.monet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
